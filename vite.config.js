19dbdc46cbe6fc4bffb0da918b3a51b5df6708b4
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-01-19 21:50:52 +0200

    202401192151

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 // import Inspect from 'vite-plugin-inspect'
 import vitePugPlugin from 'vite-plugin-pug-transformer'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
-	// plugins: [Inspect()],
+// export default {
+// 	// plugins: [Inspect()],
+// 	plugins: [vitePugPlugin()],
+// }
+
+export default defineConfig({
 	plugins: [vitePugPlugin()],
-}
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				nested: resolve(__dirname, 'pages/question.html'),
+			},
+		},
+	},
+})
