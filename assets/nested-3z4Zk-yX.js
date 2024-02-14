20a41ea8e45dab48bf20c0e56d353ea15e6d1e53
djtopolevskiy <112426198+djtopolevskiy@users.noreply.github.com>
2024-02-14 22:23:14 +0200

    Updates

---
+++ assets/nested-3z4Zk-yX.js
@@ -0,0 +1,1 @@
+import{B as e,P as a,A as s,a as o}from"./spoilers-GEhGO48o.js";e.checkWebpSupport();e.addTouchClass();e.headerFixed();new a;new s(".accordion",{shouldOpenAll:!1,defaultOpen:[],collapsedClass:"open"});new o().init();
